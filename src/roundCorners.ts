a925e7bbc0ecf900ea816c553ef21cec79b7817f
Ruby Moore <rupertofly@ruby-mbp.local>
2019-06-23 19:23:38 +1000

    fingersCrosseditWorks
    Signed-off-by: Ruby Moore <rupertofly@ruby-mbp.local>

--- src/roundCorners.ts
+++ src/roundCorners.ts
@@ -1,22 +1,3 @@
-/*****************************************************************************
-*                                                                            *
-*  SVG Path Rounding Function                                                *
-*  Copyright (C) 2014 Yona Appletree                                         *
-*                                                                            *
-*  Licensed under the Apache License, Version 2.0 (the "License");           *
-*  you may not use this file except in compliance with the License.          *
-*  You may obtain a copy of the License at                                   *
-*                                                                            *
-*      http://www.apache.org/licenses/LICENSE-2.0                            *
-*                                                                            *
-*  Unless required by applicable law or agreed to in writing, software       *
-*  distributed under the License is distributed on an "AS IS" BASIS,         *
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
-*  See the License for the specific language governing permissions and       *
-*  limitations under the License.                                            *
-*                                                                            *
-*****************************************************************************/
-
 /**
  * SVG Path rounding function. Takes an input path string and outputs a path
  * string where all line-line corners have been rounded. Only supports absolute
@@ -164,5 +145,5 @@ export function roundPathCorners(pathString: string, radius: number, useFraction
     resultCommands = commands;
   }
   
-  return resultCommands.reduce(function(str, c){ return str + c.join(" ") + " "; }, "") as string;
+  return resultCommands.reduce(function(str, c){ return str + c.join(" ") + " "; }, "") as any;
 }
