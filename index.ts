8f9c5d64e927835e18afff6425b401c35ee09d2e
Ruby Moore <rupertofly@ruby-mbp.local>
2019-06-23 19:15:00 +1000

    minorchanges
    Signed-off-by: Ruby Moore <rupertofly@ruby-mbp.local>

--- index.ts
+++ index.ts
@@ -4,5 +4,6 @@ export * from './src/MyPolygon';
 export * from './src/bSpline';
 export * from './src/JSClipperHelper';
 export * from './src/utils';
+export * from './src/roundCorners'
 import noise from './src/noise';
 export const Noise = noise;
