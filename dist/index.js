bb4321d2a3d5070d36899507e86eb08462b3ab27
Ruby Quail <ruby@rubyquail.design>
2020-01-10 17:05:31 +1100

    added some fun exports

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,9 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
 var roundCorners_1 = require("./roundCorners");
 exports.myRoundPolly = roundCorners_1.myRoundPolly;
+__export(require("./transformMultiPG"));
+__export(require("./drawMultiPG"));
